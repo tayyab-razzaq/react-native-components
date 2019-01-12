7c0a105b4f1db4b411af0be0ab5c8b4b64c5037d
tayyab <tayyab.razzaq@arbisoft.com>
2019-01-12 19:37:21 +0500

    Feat: Utils Added
    basic utils for calendar, color and helpers are added
    Updated HomeScreen code to check HMR on device

---
+++ utils/_calendar.js
@@ -0,0 +1,57 @@
+// utils/_calendar.js
+
+import {AsyncStorage} from 'react-native'
+import {getMetricMetaInfo, timeToString} from './helpers'
+
+export const CALENDAR_STORAGE_KEY = 'UdaciFitness:calendar';
+
+function getRandomNumber(max) {
+	return Math.floor(Math.random() * max);
+}
+
+function setDummyData() {
+	const {run, bike, swim, sleep, eat} = getMetricMetaInfo();
+	
+	let dummyData = {};
+	const timestamp = Date.now();
+	
+	for (let i = -183; 0 > i; i++) {
+		const time = timestamp + i * 24 * 60 * 60 * 1000;
+		const strTime = timeToString(time);
+		dummyData[strTime] = 0 === getRandomNumber(3) % 2
+			? {
+				run: getRandomNumber(run.max),
+				bike: getRandomNumber(bike.max),
+				swim: getRandomNumber(swim.max),
+				sleep: getRandomNumber(sleep.max),
+				eat: getRandomNumber(eat.max),
+			}
+			: null;
+	}
+	
+	AsyncStorage.setItem(CALENDAR_STORAGE_KEY, JSON.stringify(dummyData));
+	
+	return dummyData;
+}
+
+function setMissingDates(dates) {
+	const length = Object.keys(dates).length;
+	const timestamp = Date.now();
+	
+	for (let i = -183; 0 > i; i++) {
+		const time = timestamp + i * 24 * 60 * 60 * 1000;
+		const strTime = timeToString(time);
+		
+		if ('undefined' === typeof dates[strTime]) {
+			dates[strTime] = null;
+		}
+	}
+	
+	return dates;
+}
+
+export function formatCalendarResults(results) {
+	return null === results
+		? setDummyData()
+		: setMissingDates(JSON.parse(results));
+}
